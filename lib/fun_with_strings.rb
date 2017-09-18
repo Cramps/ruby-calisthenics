e0a690bc134c98c632f7b47aa99783e4b1233e65
Louis <louis.mejiar@gmail.com>
2017-09-18 01:15:06 +0000

    Finish part 1.

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,12 +1,35 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+    self.downcase.gsub(/\W/, '') == self.downcase.gsub(/\W/,'').reverse
   end
   def count_words
-    # your code here
+    count = Hash.new(0)
+    words = self.split(/\W|\b/)
+    words.each do |w|
+      count[w.downcase] += 1 unless w.empty?
+    end
+    count
   end
   def anagram_groups
-    # your code here
+    output = []
+    unless self.empty?
+      self.split(/\b/).each do |w|
+        added = false
+        output.each do |g|
+          if g.empty? || w.anagram?(g[0])
+            g.push(w)
+            added = true
+          end
+          break if added
+        end
+        output.push [w] unless added
+      end
+    end
+    output
+  end
+  
+  def anagram? other
+    self.downcase.chars.sort == other.downcase.chars.sort
   end
 end
 
